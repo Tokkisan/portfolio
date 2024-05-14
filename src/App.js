0b97bf2d25a062187e3ab276f41523a780205bbd
Tokkisa <aaron-k-lee.csu@fullerton.edu>
2024-05-14 00:18:14 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  // Function to scroll to the top of the page
+const goToTop = () => {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+    });
+};
+
+return (
     <div>
-        <div className="topnav">
+    <div className="topnav">
         <a href="#top"> Home </a>
         <a href="#projects"> Projects </a>
-        <a href="https://github.com/Tokkisan" target="_blank" style={{ float: 'right', paddingRight: '30px' }}>
-            <i className="fa fa-github"></i>
+        <a href="https://github.com/Tokkisan" target="_blank" rel="noreferrer" style={{ float: 'right', paddingRight: '30px' }}>
+        <i className="fa fa-github"></i>
         </a>
-        <a href="https://www.linkedin.com/in/aaron-lee-70b7b9239/" target="_blank" style={{ float: 'right' }}>
-            <i className="fa fa-linkedin"></i>
+        <a href="https://www.linkedin.com/in/aaron-lee-70b7b9239/" target="_blank" rel="noreferrer" style={{ float: 'right' }}>
+        <i className="fa fa-linkedin"></i>
         </a>
-        </div>
-
-
-        <div className="bottombar" id ="bottombar">
-            <button onClick="{goToTop()}" id="myBtn" class="top_button"> Top </button>
-        </div>
     </div>
-  );
+
+    <div className="bottombar" id="bottombar">
+        <button onClick={goToTop} id="myBtn" className="top_button"> Top </button>
+    </div>
+    </div>
+);
 }
 
 export default App;
